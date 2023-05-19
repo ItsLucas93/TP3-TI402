326bf7e48b3d18e101a9f1f353e5bbff729dbf29
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2023-05-19 14:26:17 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,64 +12,69 @@ var carspecial = "%!&*^()#$:";
 
 function generer(){
     var monformulaire = document.forms.ajoutPWD;
-    var password = "";
-    var listecar= "";
 
-    if (monformulaire.elements["minuscule"].checked){
-        listecar+=minuscule;
-    }
-    if (monformulaire.elements["majuscule"].checked){
-        listecar+=majuscule;
-    }
-    if (monformulaire.elements["chiffre"].checked){
-        listecar+=chiffre;
-    }
-    if (monformulaire.elements["symbole"].checked){
-        listecar+=carspecial;
-    }
+    if (assertRequiredField(monformulaire)) {
+        var password = "";
+        var listecar= "";
 
-    while(verify(password,monformulaire)===false){
-        password="";
-        console.log(password,verify(password,monformulaire));
-        for (var i=1; i <= monformulaire.elements["nombrecar"].value;i++){
-            var randomNumber = Math.floor(Math.random()*listecar.length);
-            password+=listecar.substring(randomNumber,randomNumber+1);
-            
+        if (monformulaire.elements["minuscule"].checked){
+            listecar+=minuscule;
         }
+        if (monformulaire.elements["majuscule"].checked){
+            listecar+=majuscule;
+        }
+        if (monformulaire.elements["chiffre"].checked){
+            listecar+=chiffre;
+        }
+        if (monformulaire.elements["symbole"].checked){
+            listecar+=carspecial;
+        }
+
+        while(verify(password,monformulaire)===false){
+            password="";
+            console.log(password,verify(password,monformulaire));
+            for (var i=1; i <= monformulaire.elements["nombrecar"].value;i++){
+                var randomNumber = Math.floor(Math.random()*listecar.length);
+                password+=listecar.substring(randomNumber,randomNumber+1);
+
+            }
+        }
+
+        /*console.log(monformulaire.nombrecar.value);*/
+
+        var newLine = document.createElement("tr");
+
+        /*var col1 = document.createElement("td");
+        var col2 = document.createElement("td");
+        var col3 = document.createElement("td");
+        var col4 = document.createElement("td");
+        //var col5 = document.createElement("td");
+
+        col1.textContent="8";
+        col2.textContent="08/01/2003";
+        col3.textContent="site";
+        col4.textContent="netflix";
+        //col5.textContent="5(_ofsd)fgv";*/
+
+        var nbcar = document.createElement("td");
+        var date = document.createElement("td");
+        var catego = document.createElement("td");
+        var siteappli = document.createElement("td");
+        var finalpassword = document.createElement("td");
+
+        nbcar.textContent = monformulaire.elements["nombrecar"].value;
+        date.textContent = monformulaire.elements["date"].value;
+        catego.textContent = monformulaire.elements["categorie"].value;
+        siteappli.textContent = monformulaire.elements["siteapp"].value;
+        finalpassword.textContent = password;
+
+        newLine.append(nbcar, date, catego, siteappli, finalpassword);
+
+        var pwdTab = document.getElementById("montab");
+
+        pwdTab.appendChild(newLine);
     }
-
-    /*console.log(monformulaire.nombrecar.value);*/
-
-    var newLine = document.createElement("tr");
-
-    /*var col1 = document.createElement("td");
-    var col2 = document.createElement("td");
-    var col3 = document.createElement("td");
-    var col4 = document.createElement("td");
-    //var col5 = document.createElement("td");
-
-    col1.textContent="8";
-    col2.textContent="08/01/2003";
-    col3.textContent="site";
-    col4.textContent="netflix";
-    //col5.textContent="5(_ofsd)fgv";*/
-
-    var nbcar = document.createElement("td");
-    var date = document.createElement("td");
-    var catego = document.createElement("td");
-    var siteappli = document.createElement("td");
-
-    nbcar.textContent = monformulaire.elements["nombrecar"].value;
-    date.textContent = monformulaire.elements["date"].value;
-    catego.textContent = monformulaire.elements["categorie"].value;
-    siteappli.textContent = monformulaire.elements["siteapp"].value;
-
-    newLine.append(nbcar,date,catego,siteappli,password);
-
-    var pwdTab = document.getElementById("montab");
-
-    pwdTab.appendChild(newLine);
-
+    else {alert("Champs incomplets. Veuillez remplir les champs et cocher l'une des cases.");}
 }
 
 
@@ -101,3 +106,6 @@ function hasCharacter(string1, string2) {
     return false;
 }
 
+function assertRequiredField(monformulaire){
+    return (monformulaire.elements["minuscule"].checked || monformulaire.elements["majuscule"].checked || monformulaire.elements["chiffre"].checked || monformulaire.elements["symbole"].checked) && (monformulaire.elements["nombrecar"].value !== "" && monformulaire.elements["date"].value !== "dd/mm/yyyy" && monformulaire.elements["categorie"].value !== "" && monformulaire.elements["categorie"].value !== "" && monformulaire.elements["siteapp"].value !== "");
+}
