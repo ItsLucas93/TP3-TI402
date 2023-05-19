0b5de16b64378852ed2aef6059d96b591f5774ea
Kenix <antoinebd.pro@gmail.com>
2023-05-19 13:10:53 +0200

    generer5

--- js/script.js
+++ js/script.js
@@ -8,12 +8,23 @@ setInterval(bienvenue,5000)*/
 function generer(){
     /*let monformulaire = document.forms.ajoutPWD;
     console.log(monformulaire.nombrecar.value);*/
-    let newLine = document.createElement("tr");
-    let col1 = document.createElement("td").textContent="AZERTY";
-    let col2 = document.createElement("td").textContent="QSDJHJKFG";
-    let col3 = document.createElement("td").textContent="65S4DFG";
-    let col4 = document.createElement("td").textContent="2354KJRHH";
-    let col5 = document.createElement("td").textContent="5(_ofsd)fgv";
-    newLine.append(col1,col2,col3);
-    console.log(newLine);
+    var newLine = document.createElement("tr");
+
+    var col1 = document.createElement("td");
+    var col2 = document.createElement("td");
+    var col3 = document.createElement("td");
+    var col4 = document.createElement("td");
+    var col5 = document.createElement("td");
+
+    col1.textContent="8";
+    col2.textContent="08/01/2003";
+    col3.textContent="site";
+    col4.textContent="netflix";
+    col5.textContent="5(_ofsd)fgv";
+
+    newLine.append(col1,col2,col3,col4,col5);
+
+    var pwdTab = document.getElementById("montab");
+    
+    pwdTab.appendChild(newLine);
 }
