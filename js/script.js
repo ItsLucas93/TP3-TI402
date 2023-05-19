aa7294724622d00e971537bde22be4b0fac7e858
Kenix <antoinebd.pro@gmail.com>
2023-05-19 13:34:57 +0200

    challenge

--- js/script.js
+++ js/script.js
@@ -28,16 +28,22 @@ function generer(){
         listecar+=symbole;
     }
 
-    for (var i=1; i <= monformulaire.elements["nombrecar"].value;i++){
-        var randomNumber = Math.floor(Math.random()*listecar.length);
-        password+=listecar.substring(randomNumber,randomNumber+1);
+    while(verify(password,monformulaire)===false){
+        console.log(password,verify(password,monformulaire));
+        for (var i=1; i <= monformulaire.elements["nombrecar"].value;i++){
+            var randomNumber = Math.floor(Math.random()*listecar.length);
+            password+=listecar.substring(randomNumber,randomNumber+1);
+            
+        }
     }
+
+    
 
     /*console.log(monformulaire.nombrecar.value);*/
 
     var newLine = document.createElement("tr");
 
-    var col1 = document.createElement("td");
+    /*var col1 = document.createElement("td");
     var col2 = document.createElement("td");
     var col3 = document.createElement("td");
     var col4 = document.createElement("td");
@@ -47,9 +53,9 @@ function generer(){
     col2.textContent="08/01/2003";
     col3.textContent="site";
     col4.textContent="netflix";
-    //col5.textContent="5(_ofsd)fgv";
+    //col5.textContent="5(_ofsd)fgv";*/
 
-    newLine.append(col1,col2,col3,col4,password);
+    newLine.append(password);
 
     var pwdTab = document.getElementById("montab");
 
@@ -83,7 +89,7 @@ function verify(password, monformulaire) {
 
 
 function hasCharacter(string1, string2) {
-    for (var i = 0; i < string1.length; i++) {if (string2.includes(string1[i])) {return true;}}
+    for (var i = 0; i < string2.length; i++) {if (string1.includes(string2[i])) {return true;}}
     return false;
 }
 
