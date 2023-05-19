a01cbaca4dea85f4d0f229574dfbbd7e17dc458c
Kenix <antoinebd.pro@gmail.com>
2023-05-19 12:59:10 +0200

    generer5d

--- js/script.js
+++ js/script.js
@@ -6,7 +6,14 @@
 setInterval(bienvenue,5000)*/
 
 function generer(){
-    let monformulaire = document.forms.ajoutPWD;
-    console.log(monformulaire.nombrecar.value);
+    /*let monformulaire = document.forms.ajoutPWD;
+    console.log(monformulaire.nombrecar.value);*/
     let newLine = document.createElement("tr");
+    let col1 = document.createElement("td").textContent="AZERTY";
+    let col2 = document.createElement("td").textContent="QSDJHJKFG";
+    let col3 = document.createElement("td").textContent="65S4DFG";
+    let col4 = document.createElement("td").textContent="2354KJRHH";
+    let col5 = document.createElement("td").textContent="5(_ofsd)fgv";
+    newLine.append(col1,col2,col3);
+    console.log(newLine);
 }
