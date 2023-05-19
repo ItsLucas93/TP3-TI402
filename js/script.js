cc41bd2b4c889e995865dc8e2bfbcbb4502ba0eb
Kenix <antoinebd.pro@gmail.com>
2023-05-19 12:25:55 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-function bienvenue(){
+/*function bienvenue(){
     alert("Hello world !");
-    setTimeout(bienvenue,2000)
+    /*setTimeout(bienvenue,2000)
+    setInterval(bienvenue,5000)
 }
+setInterval(bienvenue,5000)*/
 
